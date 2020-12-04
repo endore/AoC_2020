b5d42aab5772d24b022f2625d7dd4d8069fff90c
Juan Contreras <js.contreras@hotmail.com>
2020-12-03 19:31:15 -0500

    day 3 - part 2 complete

--- day3/day3.jl
+++ day3/day3.jl
@@ -7,22 +7,60 @@ function numTrees(input)
     n_rows = length(input)
     n_cols = length(input[1])
 
-    # expand pattern so that n_cols >= 3 * n_rows
-    n_repeats = Int(ceil(3*n_rows/n_cols))
+    # expand pattern so that n_cols >= down_steps * right_size
+
+    start_row = 1
+    start_col = 1
+
+    down_size = 1
+    right_size = 3
+
+    down_steps = Int((n_rows-1)/down_size)
+    n_repeats = Int(ceil((1+right_size*down_steps)/n_cols))
     expanded_input = [repeat(row,n_repeats) for row in input]
 
     #traverse map and count trees
     n_trees = 0
 
-    start_row = 1
-    start_col = 1
-    down_size = 1
-    right_size = 3
 
     current_row = start_row
     current_col = start_col
 
-    for step  in 1:n_rows-1
+
+    for step in 1:down_steps
+        current_row = current_row + down_size
+        current_col = current_col + right_size
+
+        if expanded_input[current_row][current_col] == '#'
+            n_trees += 1
+        end
+    end
+
+    return n_trees
+end
+
+function numTrees(input, right_size, down_size)
+    # get num rows, cols
+    n_rows = length(input)
+    n_cols = length(input[1])
+
+    # expand pattern so that n_cols >= down_steps * right_size
+    start_row = 1
+    start_col = 1
+
+    down_steps = Int((n_rows-1)/down_size)
+    n_repeats = Int(ceil((1+right_size*down_steps)/n_cols))
+    expanded_input = [repeat(row,n_repeats) for row in input]
+
+    #traverse map and count trees
+    n_trees = 0
+
+
+    current_row = start_row
+    current_col = start_col
+
+
+    for step in 1:down_steps
         current_row = current_row + down_size
         current_col = current_col + right_size
 
