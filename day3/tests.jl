783f8b0521be8c3e39327e15d5f3f5fdc5d03094
Juan Contreras <js.contreras@hotmail.com>
2020-12-05 06:50:27 -0500

    day 3 complete

--- day3/tests.jl
+++ day3/tests.jl
@@ -1,8 +1,8 @@
 include("day3.jl")
 using Test
 
-real_input = getInput(3,String)
 
+real_input = getInput(3,String)
 test_input = [
 "..##......."
 "#...#...#.."
@@ -21,14 +21,31 @@ test_input = [
     @test numTrees(test_input) == 7
 end
 
+@testset "Example 1" begin
+    @test numTrees(test_input, 3, 1) == 7
+end
+
 @testset "Part 1" begin
     @test numTrees(real_input) == 216
 end
 
+@testset "Part 1" begin
+    @test numTrees(real_input, 3, 1) == 216
+end
+
 @testset "Example 2" begin
-    @test numValid2(test_input) == 1
+    @test numTrees(test_input,1,1) == 2
+    @test numTrees(test_input,3,1) == 7
+    @test numTrees(test_input,5,1) == 3
+    @test numTrees(test_input,7,1) == 4
+    @test numTrees(test_input,1,2) == 2
 end
 
 @testset "Part 2" begin
-    @test numValid2(real_input) == 404
+    @test numTrees(real_input,1,1) == 79
+    @test numTrees(real_input,3,1) == 216
+    @test numTrees(real_input,5,1) == 91
+    @test numTrees(real_input,7,1) == 96
+    @test numTrees(real_input,1,2) == 45
+    @test 79*216*91*96*45 == 6708199680
 end
