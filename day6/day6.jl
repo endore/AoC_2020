38874b2621b43cd39d99ae2def63eb69bff9bd74
Juan Contreras <js.contreras@hotmail.com>
2020-12-06 09:18:09 -0500

    day 6 complete

--- day6/day6.jl
+++ day6/day6.jl
@@ -14,3 +14,20 @@ function getGroups(input)
 
     return groups
 end
+
+function getGroups2(input)
+    # remove last newline character form input
+    input = input[1:(end-1)]
+
+    # parse text data into discrete array of candidates
+    groups = split(input,"\n\n")
+
+    # remove \n within each group
+    groups = Array{String,1}[split(group,"\n") for group in groups]
+
+    return groups
+end
+
+function getInteresectCount(group)
+    return length(intersect(group...))
+end
--- day6/test.jl
+++ day6/test.jl
@@ -31,16 +31,37 @@ test_input_group = [
 "b"
 ]
 
+
+test_input_group2 = Array{String,1}[]
+push!(test_input_group2,["abc"])
+push!(test_input_group2,["a";"b";"c"])
+push!(test_input_group2,["ab"; "ac"])
+push!(test_input_group2,["a";"a";"a";"a"])
+push!(test_input_group2,["b"])
+
+
 @testset "Test1: getGroups" begin
     @test getGroups(test_input) == test_input_group
 end
-
 
 @testset "Sample 1" begin
     @test sum(length.(unique.(test_input_group))) == 11
 end
 
-@testset "Part 1" begin
+@testset "Part 1: answer" begin
     groups = getGroups(real_input)
-    @test sum(length.(unique.(groups))) == 11
+    @test sum(length.(unique.(groups))) == 6878
+end
+
+@testset "Part 2: test getGroups for part 2" begin
+    @test getGroups2(test_input) == test_input_group2
+end
+
+@testset "Part 2: test answer" begin
+    @test sum(getInteresectCount.(test_input_group2)) == 6
+end
+
+@testset "Part 2: answer" begin
+    groups = getGroups2(real_input)
+    @test sum(getInteresectCount.(groups)) == 3464
 end
